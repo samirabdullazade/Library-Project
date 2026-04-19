c12275a908330e6e43baf448844f4e718cd65ad8
samirabdullazade <samirabdullazad4@gmail.com>
2026-04-19 22:46:53 +0400

    tam kod yuklendi

--- commit.py
+++ commit.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+
 app_name = 'library'
 version = '0.1.0'
 is_active = True
@@ -13,7 +16,7 @@ def is_classic(book):
         return True
     else:
         return False
-book1=create_book("Holes","1998","Unknown")
+book1=create_book("Holes",1998,"Unknown")
 print(is_classic(book1))
 
 library=[]
@@ -31,3 +34,54 @@ def all_genres():
     for book in library:
         genres.add(book["game"])
     return genres
+
+classic_books=[book["title"] for book in library if book['year'] < 1950]
+authors=[book["author"] for book in library]
+
+def book_iterator(genre_filler=None):
+    for book in library:
+        if genre_filler is None or book["game"] == genre_filler:
+            yield book
+
+def save_library(filename="file.json"):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(library, file, indent=2)
+
+try:
+    with open("file.json", "r", encoding="utf-8") as file:
+        library = json.load(file)
+except FileNotFoundError:
+    print("No existing library found. Starting with an empty library.")
+
+now = datetime.datetime.now()
+print(f"Current date and time: {now}")
+
+week_later = now + datetime.timedelta(days=7)
+print(week_later)
+
+class Book:
+    def __init__(self, title, year, author, genre, is_read):
+        self.title = title
+        self.year = year
+        self.author = author
+        self.genre = genre
+        self.is_read = is_read
+
+    def mark_as_read(self):
+        self.is_read = True
+
+    def is_classic(self):
+        if self.year < 1950:
+            return True
+book1=Book("O Alquimista", 1988, "Paulo Coelho", "Ficção", False)
+
+class Ebook(Book):
+    def __init__(self, title, year, author, genre, filesize, is_read=False):
+        super().__init__(title, year, author, genre, is_read)
+        self.filesize = filesize
+
+class AudioBook(Book):
+    def __init__(self, title, year, author, genre, narrator, duration, is_read=False):
+        super().__init__(title, year, author, genre, is_read)
+        self.narrator = narrator
+        self.duration = duration
